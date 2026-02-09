3960028e2aceb2e7d2ab0c4cc20e1ab6aa92b678
nithin <91445424+nannithin@users.noreply.github.com>
2026-02-09 13:57:22 +0530

    Update axios.js

--- my-app/src/lib/axios.js
+++ my-app/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: false,
+  baseURL: "https://jewe-2w4e.onrender.com/api",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
