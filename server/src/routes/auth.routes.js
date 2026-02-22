a558542c82d642289edf70d5fc6c9c36cfc3e0d7
nannithin <nithinyalakala@gmail.com>
2026-02-22 18:20:10 +0530

    .

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { register, login } from "../controllers/auth.controller.js";
 import User from "../models/User.model.js";
+import { protect } from "../middleware.js";
 
 const router = express.Router();
 router.post("/register", register);
@@ -8,6 +9,7 @@ router.post("/login", login);
 
 router.get("/me", protect, async (req, res) => {
   try {
+    
     const user = await User.findById(req.user.id).select("-password");
 
     if (!user) {
