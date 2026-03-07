7285b11255097692d11c109ce8385fa4a934d0d2
nannithin <nithinyalakala@gmail.com>
2026-03-07 13:59:46 +0530

    .

--- my-app/src/app/orders/[id]/page.jsx
+++ my-app/src/app/orders/[id]/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import api from "@/lib/axios";
 import Image from "next/image";
 import { useParams } from "next/navigation";
-import { Check, CreditCard, Package } from "lucide-react";
+import { Check, CreditCard, MapPin, Package } from "lucide-react";
 
 export default function OrderDetails() {
   const { id } = useParams();
