4156c7680983fb2b3e6505e9129444563ceddac9
nannithin <nithinyalakala@gmail.com>
2026-03-18 19:16:27 +0530

    .

--- my-app/src/components/cardd.jsx
+++ my-app/src/components/cardd.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 import necklace from "../../public/necklace.jpg";
 import useStore from "@/store/useStore";
 
-const ItemCard = ({ product }) => {
+const ItemCard = ({ item : product }) => {
     const { wishlist, addToWishlist, removeFromWishlist } = useStore();
     console.log(wishlist);
-    
-    
+
+
     const isWishlisted = wishlist.some((item) => item._id === product._id);
 
     const handleWishlist = () => {
@@ -19,34 +19,67 @@ const ItemCard = ({ product }) => {
         }
     };
     return (
-        <Link href={`/item/${product.slug}`}>
+        <div
+            key={item._id}
+            onClick={() => router.push(`/item/${item.slug}`)}
+            className="group flex flex-col overflow-hidden  bg-white transition-all duration-300 cursor-pointer"
+        >
+            {/* Image */}
+            <div className="relative aspect-[4/5] w-full overflow-hidden bg-neutral-100">
+                <Image
+                    src={item.images?.[0] || "/placeholder.png"}
+                    alt={item?.title}
+                    fill
+                    sizes="(max-width: 768px) 100vw, 33vw"
+                    className="object-cover transition-transform duration-500 group-hover:scale-110"
+                />
 
-            <div>
-                <div className="relative z-10 h-32 w-32 md:h-40 md:w-40 bg-[#F6F6F6]">
-                    <Image
-                        className="object-fill"
-                        src={product.images?.[0]}
-                        alt={product.title}
-                        fill
-                    />
-                    <Heart onClick={(e) => {
+                {/* Discount badge */}
+                {item.originalPrice > item.price && (
+                    <span className="absolute top-0 left-0 bg-[#B5947C] text-white text-xs px-2.5 py-1 shadow-sm">
+                        {Math.round(
+                            ((item.originalPrice - item.price) / item.originalPrice) * 100
+                        )}
+                        % OFF
+                    </span>
+                )}
+                <Heart onClick={(e) => {
                         e.preventDefault();      // stops Link navigation
                         e.stopPropagation();     // stops bubbling
                         handleWishlist();
                     }} size={18} className={`absolute top-3 right-3 font-normal ${isWishlisted && "fill-[#B5947C] text-[#B5947C]"}`} />
-                </div>
+            </div>
 
-                <div className="w-32 md:w-40 flex flex-col items-center">
-                    <p className="text-sm text-muted-foreground uppercase">
-                        {product.category}
-                    </p>
+            {/* Content */}
+            <div className="flex flex-col justify-between gap-2 mt-2 flex-1">
+                {/* Title */}
+                <h3 className="font-medium text-[17px] text-gray-500 line-clamp-2 leading-snug">
+                    {item?.title}
+                </h3>
 
-                    <p className="line-clamp-1">{product.title}</p>
+                {/* Price + Button */}
+                <div className="flex flex-col gap-2 justify-between">
+                    <div className="flex items-center gap-3">
+                        <span className="text-base font-semibold text-[#B5947C]">
+                            ₹{item.price}
+                        </span>
 
-                    <p>₹{product.price}</p>
+                        {item.originalPrice > item.price && (
+                            <span className="text-base font-medium text-gray-400 line-through">
+                                ₹{item.originalPrice}
+                            </span>
+                        )}
+                    </div>
+
+                    <Button
+
+                        className={"w-full text-[17px]"}
+                    >
+                        BUY NOW
+                    </Button>
                 </div>
             </div>
-        </Link>
+        </div>
     );
 };
 
